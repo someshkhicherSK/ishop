9291e118b484c8cbd1ee1f2c4a6827afc56f880c
Somesh khichar <someshkhicher111@gmail.com>
2026-05-18 20:26:27 +0530

    Update page.jsx

--- frontend/src/app/(admin_website)/admin/brands/page.jsx
+++ frontend/src/app/(admin_website)/admin/brands/page.jsx
@@ -1,5 +1,4 @@
 "use client";
-// import { Edit, Trash2, RefreshCcw, Plus } from "lucide-react";
 // import Link from "next/link";
 // import { getBrands } from "../../../../../library/api_calls";
 // import { useEffect, useState } from "react";
