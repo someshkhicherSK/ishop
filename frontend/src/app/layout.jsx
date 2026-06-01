6c61ba795df62c2abee6265746a80f16e6d79bd7
Somesh Khicher <someshkhicher111@gmail.com>
2026-06-01 09:23:35 +0530

    remove StoreProvider test

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,13 +1,29 @@
-import StoreProvider from "./(user_website)/components/StoreProvider";
+// import StoreProvider from "./(user_website)/components/StoreProvider";
+// import { SpeedInsights } from "@vercel/speed-insights/next";
+// import "./globals.css";
+// export default function RootLayout({ children }) {
+//   return (
+//     <html lang="en">
+//       <body suppressHydrationWarning>
+//         <StoreProvider>
+//         {children}
+//         </StoreProvider>
+//         <SpeedInsights />
+//       </body>
+//     </html>
+//   );
+// }
+
+
+
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import "./globals.css";
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning>
-        <StoreProvider>
+      <body>
         {children}
-        </StoreProvider>
         <SpeedInsights />
       </body>
     </html>
